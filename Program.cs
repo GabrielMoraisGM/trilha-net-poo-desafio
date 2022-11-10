73288897629d31f4b467f3254928ead04c577aad
Gabriel Morais <gabriel.m.g.morais@gmail.com>
2022-11-09 23:01:53 -0300

    Adicionado propriedades, hierarquia e testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone1 = new Iphone("13", "SE", "351290372", 3);
+iphone1.InstalarAplicativo("winrar");
+iphone1.Ligar();
+iphone1.ReceberLigacao();
+
+Nokia nokia1 = new Nokia("2125939071", "Lumia", "3559102386", 6);
+nokia1.InstalarAplicativo("winrar");
+nokia1.Ligar();
+nokia1.ReceberLigacao();
